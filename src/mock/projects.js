d2faaa98d1d401d8a5cb4194810dc8fc8dbd08f7
Elian Palma <85314979+ElianPalma@users.noreply.github.com>
2026-03-21 11:36:33 -0400

    Update projects.js

--- src/mock/projects.js
+++ src/mock/projects.js
@@ -48,6 +48,7 @@ import p47 from '../components/img/loulavie.png';
 import p48 from '../components/img/miami.png';
 import p49 from '../components/img/vizzio.png';
 
+
 const projects = [
   {
     id: 1,
